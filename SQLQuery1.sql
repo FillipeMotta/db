7cc17b27a8718111b4831729f1ac5824c1fba4f8
FillipeMotta <124511366+FillipeMotta@users.noreply.github.com>
2023-09-16 00:21:25 -0300

    up
    I uploaded my database developed in the course

---
+++ SQLQuery1.sql
@@ -0,0 +1,15 @@
+select * from offer;
+insert into buyer values ('cfddff', 'mdfsdf', 12324567, 'odff', 'bdfdf','pdfdfdf', 123456);
+insert into buyer values ('adfdf', 'xdfdfd', 123541567, 'pdfdf', 'gdfdf','qdfdfd', 122343456);
+
+
+insert into cidade values ('Parapua', 'Sao Paulo');
+insert into cidade values ('Osvaldo Cruz', 'Sao Paulo');
+insert into cidade values ('Santopolis do Aguapeí', 'Sao Paulo');
+
+insert into estado values ('SP', 'Sao Paulo');
+insert into estado values ('MG', 'Minas Gerais');
+insert into estado values ('BH', 'Fortaleza');
+
+insert into faixa_imovel values ('c', 12.23, 15.6);
+ select * from estado
---
+++ SQLQuery4.sql
@@ -0,0 +1,21 @@
+select * from bairro
+insert into bairro values ('Cidade')
+insert into bairro values ('Vale do Sol')
+insert into bairro values ('Ipe')
+insert into bairro values ('Centro')
+insert into bairro values ('Breguedé')
+
+select * from buyer
+insert into buyer values('Juan Espanhol', 'Rua China', 12354687, 'Osvaldo cruz', 'Centro', 'SP', 156478793)
+insert into buyer values('Mateus Juninho', 'Rua Mandragora', 1987865, 'Osvaldo cruz', 'Centro', 'SP',195498178)
+insert into buyer values('Elias Clearman', 'Rua Mitocondrias', 17754645, 'Osvaldo cruz', 'Centro', 'SP',365469751)
+
+select * from faixa_imovel
+insert into faixa_imovel values ('Casa X', 125000, 175000)
+insert into faixa_imovel values ('Casa Y', 130000, 200000)
+insert into faixa_imovel values ('Casa Z', 80000, 150000)
+
+select * from salesman
+insert into salesman values ('Shaolin', 'Rua: numero 2', 123456789, 'Osvaldo Cruz', 'Breguedé', 'SP', 5647489, 'shaolinmatadordeporco@gmail.com')
+insert into salesman values ('Flavin', 'Rua: numero 5', 985632145, 'Osvaldo Cruz', 'Centro', 'SP', 7895632, 'flavindopneu@gmail.com')
+insert into salesman values ('Mosquitão', 'Rua: numero 9', 654231895, 'Osvaldo Cruz', 'Vale do Sol', 'SP', 6532148, 'mosquitaosbp@gmail.com')
